9ebaa351a9804e7fbe1b8288c3688b301ec46206
RafyFerrer <rafaelangelferrermartinez@gmail.com>
2025-04-05 22:12:24 +0200

    Primera actualizacion
    actualizado

---
+++ PersonajeHistoricoAdivinanzaPuntuacion/scripts/puntua.js
@@ -0,0 +1,9 @@
+var intentos;
+var puntos;
+function intentosPuntos(){
+    var objetoIntentos = document.getElementById("inte");
+    var objetoPuntos = document.getElementById("aciert");
+    objetoIntentos.textContent = intentos;
+    objetoPuntos.textContent = puntos;
+
+}
